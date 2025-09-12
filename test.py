303fe8b13bc8d87d5a6713af9857bb6a93f7bac0
AB-shaikh <shaikhabubakar059@gmail.com>
2025-09-12 17:18:05 +0530

    new python file added

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Sample data
+data = {
+    "Name": ["Amit", "Sara", "John"],
+    "Age": [25, 30, 22],
+    "City": ["Delhi", "Mumbai", "London"]
+}
+
+# DataFrame create
+df = pd.DataFrame(data)
+
+print("✅ Pandas test successful!")
+print(df)
+
